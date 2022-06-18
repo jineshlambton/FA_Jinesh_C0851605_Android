acc91a0687b2eda7bd7d1c13e37883b07099b259
Jinesh <105523801+jineshlambton@users.noreply.github.com>
2022-06-18 17:30:40 -0600

    ADD UI and init database to store data

---
+++ app/src/main/java/com/example/fa_jinesh_c0851605_android/Utility.java
@@ -0,0 +1,14 @@
+package com.example.fa_jinesh_c0851605_android;
+
+public class Utility {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "placesDB";
+    public static final String TABLE_NAME = "places";
+
+    public static final String KEY_ID = "id";
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String TITLE = "title";
+
+}
