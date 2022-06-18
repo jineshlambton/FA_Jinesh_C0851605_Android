a89a13507a68e65703f27d9dd2e86686543afacb
Jinesh <105523801+jineshlambton@users.noreply.github.com>
2022-06-18 17:53:49 -0600

    Add marker on long press on map add single press on marker to edit or delete place name store marker to database with ID

--- app/src/main/java/com/example/fa_jinesh_c0851605_android/MapsActivity.java
+++ app/src/main/java/com/example/fa_jinesh_c0851605_android/MapsActivity.java
@@ -1,21 +1,33 @@
 package com.example.fa_jinesh_c0851605_android;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.fa_jinesh_c0851605_android.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
 
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener,GoogleMap.OnMarkerClickListener{
+
+    GoogleMap mMap;
+    ActivityMapsBinding binding;
+    dbHalper db;
+
+    LatLng Place = new LatLng(53.544388, -113.490929);
+    List<Marker> markerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +54,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        db = new dbHalper(this);
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMapLongClickListener(this);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        markerList = new ArrayList<>();
+        List<Place> placeList = db.getAllPlaces();
+
+        for(Place p: placeList){
+            String myInfo = "ID: " + p.getId() + " Latitude: "+ p.getPlatitude() + "Longitude"
+                    + p.getPlongitude() + " Title: " + p.getTitle();
+            Log.d("myInfo", myInfo);
+
+            markerList.add(mMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(Double.parseDouble(p.getPlatitude())
+                            ,Double.parseDouble(p.getPlongitude()))).title(p.getTitle())
+                    .zIndex( p.getId() ).snippet("By ME")     ));
+        }
+
+        markerList.add(mMap.addMarker(new MarkerOptions()
+                .position(Place).title("Edmonton")));
+
+        for(Marker m : markerList){
+
+            LatLng latLng = new LatLng(m.getPosition().latitude, m.getPosition().longitude);
+            mMap.addMarker(new MarkerOptions().position(latLng) );
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12));
+        }
+
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+
+        Intent intent = new Intent(MapsActivity.this, AddActivity.class);
+        intent.putExtra("latitude" , latLng.latitude);
+        intent.putExtra("longitude" , latLng.longitude);
+
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+
+        Intent intent = new Intent(MapsActivity.this, ShowActivity.class);
+        intent.putExtra("latitude" , marker.getPosition().latitude);
+        intent.putExtra("longitude" , marker.getPosition().longitude);
+        intent.putExtra("title" , marker.getTitle());
+        intent.putExtra("id" ,  marker.getZIndex()) ;
+        startActivity(intent);
+        return false;
+
     }
 }
